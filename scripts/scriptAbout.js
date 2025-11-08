cd202e4f70ddcc2fd6ad971e1232346fee8ee83b
kamp <lexiaky@gmail.com>
2025-11-08 17:49:11 -0500

    branching to kamp

---
+++ scripts/scriptAbout.js
@@ -0,0 +1,6 @@
+
+	document.addEventListener("keydown", function(event) {
+		if (event.key === "Enter") { // Replace "Enter" with the desired key
+		  window.location.href = "../html/index.html"; // Replace with your desired URL
+		}
+	  });
