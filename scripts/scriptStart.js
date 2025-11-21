f5337e9749937903e1512d76edbf0de1e9a78df0
kamp <lexiaky@gmail.com>
2025-11-21 13:01:55 -0500

    name

--- scripts/scriptStart.js
+++ scripts/scriptStart.js
@@ -1,6 +1,6 @@
 document.addEventListener("keydown", function(event) {
     if (event.key === "Enter" || event.key === " ") { 
-      window.location.href = "https://kamp-tl.github.io/monke-quote/html/main.html";
+      window.location.href = "https://kamp-tl.github.io/monkeQuote/html/main.html";
     }
   });
 
